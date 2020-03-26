051157951297cabb80871bb2a62dbe29c8a8c37d
Jochen Hansoul <43874512+JochenHansoulPXL@users.noreply.github.com>
2020-03-26 17:48:55 +0100

    Used the newly created BudgetPlannerImporter class.

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,6 +1,18 @@
 package be.pxl.student;
 
+import be.pxl.student.entity.Account;
+import be.pxl.student.util.BudgetPlannerImporter;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class BudgetPlanner {
+    private static final Logger logger = Logger.getLogger("BudgetPlanner");
 
-
+    public static void main(String[] args) {
+        //logger.log(Level.WARNING, "first warning log");
+        Set<Account> accounts = BudgetPlannerImporter.readFile("account_payments.csv");
+        System.out.println(accounts.toString());
+    }
 }
